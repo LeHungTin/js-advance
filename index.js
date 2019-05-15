940bbe86560c41ac10e76dd4055618de06ea7c7c
tin1806 <lehungtin.ktqs@gmail.com>
2019-05-15 14:43:25 +0700

    lession 7 - arguments

--- index.js
+++ index.js
@@ -1,14 +1,37 @@
-// Template string
+// Array-like object & arguments
 
-// function greeting(name) {
-// 	return 'Hi ' + name + '!';
+// const names = ['Tin', 'Tuan', 'Hung'];
+
+// for(let i = 0; i < names.length; i++) {
+// 	console.log(names[i]);
 // }
 
-// console.log(greeting('Tin'));
+// const obj = {
+// 	0: 'Tin',
+// 	1: 'Tuan',
+// 	2: 'Hung',
+// 	length: 3
+// };
 
-function greeting(name) {
-	return `Hi ${name} ${1 + 2}
-	${3*3}!`;
+// for(let i = 0; i < obj.length; i++) {
+// 	console.log(obj[i]);
+// }
+
+function sum() {
+	// console.log(arguments);
+	// console.log(arguments.length);
+	let result = 0;
+	for(let i = 0; i < arguments.length; i++) {
+		result += arguments[i];
+	}
+	return result;
 }
 
-console.log(greeting('Tin'));
+// function sum() {
+// 	const numbers = Array.from(arguments);
+// 	return numbers.reduce(function(sum, num) {
+// 		return sum + num;
+// 	}, 0);
+// }
+
+console.log(sum(1, 2, 3, 5, 7, 9));
