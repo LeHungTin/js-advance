4fb1183e799fa06310a33b7aa3a23e991f34b385
tin1806 <lehungtin.ktqs@gmail.com>
2019-05-15 13:39:32 +0700

    lession 4 - Arrow function expression

--- index.js
+++ index.js
@@ -1,23 +1,41 @@
-// function context and bind
+// arrow function expresstion
 
-var mouse = {
-	name: 'Mickey',
-	sayHi: function() {
-		console.log('Hi, my name is ', this.name);
-	}
-};
+// function sum(a, b) {
+// 	return a + b;
+// }
 
-// mouse.sayHi();  // function context
+// console.log(sum(2, 5));
 
-// var say = mouse.sayHi; // global context
-// say();
-
-var say = mouse.sayHi.bind(mouse);
-say();
-
-// var cat = {
-// 	name: 'Tom'
+// var sum = function(a, b) {
+// 	return a + b;
 // };
 
-// var say = mouse.sayHi.bind(cat);
-// say();
+// console.log(sum(2, 5));
+
+// var sum = (a, b) => {
+// 	return a + b;
+// }
+
+// console.log(sum(2, 5));
+
+// var sum = (a, b) => a + b;
+
+// console.log(sum(2, 5));
+
+// var square = (x) => x*x;
+
+// console.log(square(2));
+
+var arr = [1, 2, 3];
+
+var newArr = arr.map(x => x*x);
+
+console.log(newArr);
+
+// var doSomething = () => Date.now();
+
+// console.log(doSomething());
+
+//note: 
+// 1, Neu co 2 tham so hoac khong co tham so nao phai co ();
+// 2. Neu co 2 dong lenh tro len trong ham thi phai co {};
